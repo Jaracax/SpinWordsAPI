61cac4ce785fcf791a3702b8fde787d2022d7123
Jose Antonio Dominguez <jadominguez99@gmail.com>
2023-05-06 11:56:50 -0300

    initial commit

---
+++ src/main/java/com/example/SpinWordsAPI/SpinWordsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpinWordsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinWordsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinWordsApiApplication.class, args);
+	}
+
+}
