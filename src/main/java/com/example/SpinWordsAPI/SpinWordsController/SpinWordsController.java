1a7663530153668e0c500bd824da427cdd3676b7
Jose Antonio Dominguez <jadominguez99@gmail.com>
2023-05-06 12:32:13 -0300

    fix a bug where the serialization wasn't working

--- src/main/java/com/example/SpinWordsAPI/SpinWordsController/SpinWordsController.java
+++ src/main/java/com/example/SpinWordsAPI/SpinWordsController/SpinWordsController.java
@@ -1,5 +1,6 @@
 package com.example.SpinWordsAPI.SpinWordsController;
 
+import com.example.SpinWordsAPI.SentenceRequest.SentenceRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpinWordsController {
 
     @PostMapping("spinning")
-    public String spinWords(@RequestBody String sentence) {
+    public String spinWords(@RequestBody SentenceRequest request) {
+        String sentence = request.getSentence();
         String[] words = sentence.split("\\s");
         for (int i=0; i<words.length; i++) {
             if (words[i].length() >= 5) {
